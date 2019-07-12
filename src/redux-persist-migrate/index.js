TechForce-Lyron0785/zulip-mobile

commit 397d427ca21058c83b78d91fc264d2b73ba3c828

persist-migrate: Clean up to make strict-local (the last file! \o/)
This just requires a couple of changes to stop mutating function
arguments inside the function -- which, at least in the bigger of
these two cases (the outer function itself), is a significant
improvement in the code anyway.

And now every file in our app codebase (src/, excluding tests)
has at least `@flow strict-local`! 🎉 Huzzah.

src/redux-persist-migrate/index.js
@@ -1,4 +1,4 @@
-/* @flow */
+/* @flow strict-local */
 import type { Reducer, Store, Dispatch } from 'redux';
 
 import { REHYDRATE } from '../actionConstants';
@@ -16,6 +16,8 @@ const processKey = key => {
 type InnerStoreCreator<S, A, D> = (Reducer<S, A>, S | void) => Store<S, A, D>;
 type StoreEnhancer<S, A, D> = (InnerStoreCreator<S, A, D>) => InnerStoreCreator<S, A, D>;
 
+/* eslint-disable no-use-before-define */
+
 export default function createMigration(
   manifest: { [string]: (State) => State },
   versionSelector: string | (State => number | string | void),
@@ -23,8 +25,8 @@ export default function createMigration(
 ): StoreEnhancer<State, Action, Dispatch<Action>> {
   if (typeof versionSelector === 'string') {
     const reducerKey = versionSelector;
-    versionSelector = state => state && state[reducerKey] && state[reducerKey].version;
-    versionSetter = (state, version) => {
+    const realVersionSelector = state => state && state[reducerKey] && state[reducerKey].version;
+    const realVersionSetter = (state, version) => {
       if (['undefined', 'object'].indexOf(typeof state[reducerKey]) === -1) {
         logErrorRemotely(
           new Error(
@@ -37,8 +39,21 @@ export default function createMigration(
       state[reducerKey].version = version;
       return state;
     };
+    return createMigrationImpl(manifest, realVersionSelector, realVersionSetter);
   }
 
+  if (versionSetter === undefined) {
+    throw new Error('createMigration: bad arguments');
+  }
+
+  return createMigrationImpl(manifest, versionSelector, versionSetter);
+}
+
+export function createMigrationImpl(
+  manifest: { [string]: (State) => State },
+  versionSelector: State => number | string | void,
+  versionSetter: (State, number) => State,
+): StoreEnhancer<State, Action, Dispatch<Action>> {
   const versionKeys = Object.keys(manifest)
     .map(processKey)
     .sort((a, b) => a - b);
@@ -47,16 +62,14 @@ export default function createMigration(
     currentVersion = -1;
   }
 
-  const migrate = (state: State, version) => {
+  const migrate = (state, version) => {
+    let newState = state;
     versionKeys.filter(v => v > version || version === null).forEach(v => {
-      state = manifest[v.toString()](state);
+      newState = manifest[v.toString()](state);
     });
 
-    if (versionSetter === undefined) {
-      throw new Error('createMigration: bad arguments');
-    }
-    state = versionSetter(state, currentVersion);
-    return state;
+    newState = versionSetter(state, currentVersion);
+    return newState;
   };
 
   const migrationDispatch = next => (action: Action) => {

commit 5f7499a958c09e4e4296a0b4f56ce82392570523

persist-migrate: Fix to actually work again, oops.
Fixes #3553.

Looks like I broke this in 397d427ca.  The effect was that migrations
wouldn't do anything; the only thing the migrations system would do is
to update the `migrations` metadata *as if* the needed migrations had
been run.

We haven't added any migrations since then (trying to do so is how I
discovered this bug today).  The buggy commit went out in the 25.0.114
release; there were no new migrations in that release, and the
previous one was in 24.0.113, adjusting `realm.emoji`.

So the total effect is that anyone upgrading directly to 25.0.114 (or
later) from a release prior to 24.0.113 wouldn't get the migrations in
between.  That isn't common, but for the users it did affect, it could
cause a crash.  In particular, for anyone upgrading from before
22.0.107 (almost 5 months earlier), they'd miss a migration affecting
`accounts`, which unlike most of our data doesn't get regularly
clobbered by a `/register` response.

Well, fix the bug.  In an upcoming commit we'll follow up with a
remedial migration, too, to fix any affected installs that are still
using the app.

A shame this file isn't tested. :-/  That's one root cause of this bug
getting through.

src/redux-persist-migrate/index.js
@@ -65,10 +65,10 @@ export function createMigrationImpl(
   const migrate = (state, version) => {
     let newState = state;
     versionKeys.filter(v => v > version || version === null).forEach(v => {
-      newState = manifest[v.toString()](state);
+      newState = manifest[v.toString()](newState);
     });
 
-    newState = versionSetter(state, currentVersion);
+    newState = versionSetter(newState, currentVersion);
     return newState;
   };
 