TechForce-Lyron0785/zulip-mobile

commit 72280dd968a7d3287b22cff6e6d4dcffba30db10

WebLink [nfc]: Remove a few no-op ZulipText wrappers in callers' `children`
And update the jsdoc to mention the `text` prop.

The WebLink color will still be applied; note this implementation
comment:

// Some React node that isn't a React element (a `React.Element`);
// e.g., a plain string. The enclosing ZulipText will apply its
// styles directly.

I think I've thought of a better way for ZulipText's
aggressiveDefaultStyles to work, so it's less tricky to think
through what styles will get applied; coming up.

src/common/WebLink.js
@@ -23,7 +23,9 @@ const componentStyles = createStyleSheet({
 /**
  * A button styled like a web link.
  *
- * Accepts `ZulipText`s, `ZulipTextIntl`s, and strings as children.
+ * Accepts strings as children or a `text` prop, just like ZulipText.
+ *
+ * Also accepts `ZulipText`s and `ZulipTextIntl`s as children.
  *
  * Note: This sort of messes in those non-string children's business by
  *   unsetting some of their default style attributes. It does this so that

src/account-info/CustomProfileFields.js
@@ -70,9 +70,7 @@ function CustomProfileFieldRow(props: {|
       valueElement = (
         <View style={styles.valueView}>
           {value.url ? (
-            <WebLink url={value.url}>
-              <ZulipText text={value.text} />
-            </WebLink>
+            <WebLink url={value.url} text={value.text} />
           ) : (
             <ZulipText text={value.text} />
           )}

src/message/ReadReceiptsScreen.js
@@ -24,7 +24,6 @@ import type { UserOrBot } from '../api/modelTypes';
 import LoadingIndicator from '../common/LoadingIndicator';
 import WebLink from '../common/WebLink';
 import { createStyleSheet } from '../styles';
-import ZulipText from '../common/ZulipText';
 
 type Props = $ReadOnly<{|
   navigation: AppNavigationProp<'read-receipts'>,
@@ -138,11 +137,7 @@ export default function ReadReceiptsScreen(props: Props): Node {
             values: {
               num_of_people: displayUserIds.length,
               'z-link': chunks => (
-                <WebLink url={new URL('/help/read-receipts', auth.realm)}>
-                  {chunks.map(chunk => (
-                    <ZulipText>{chunk}</ZulipText>
-                  ))}
-                </WebLink>
+                <WebLink url={new URL('/help/read-receipts', auth.realm)}>{chunks}</WebLink>
               ),
             },
           }

commit 16f342663c6190f59e2b264de4546e466101a1cc

WebLink: Forward text prop to ZulipText, fixing CustomProfileFields crash
This fixes a regression introduced in 72280dd96, released in the
v27.198 beta. The underlying ZulipText was throwing its error

  `text` or `children` should be non-nullish

when the user navigated to a profile screen that tried to render a
"link" type custom profile field, such as a GitHub link.

src/common/WebLink.js
@@ -7,9 +7,20 @@ import { openLinkWithUserPreference } from '../utils/openLink';
 import { BRAND_COLOR, createStyleSheet } from '../styles';
 import { useGlobalSelector } from '../react-redux';
 import { getGlobalSettings } from '../directSelectors';
+import type { BoundedDiff } from '../generics';
+
+type ZulipTextProps = $Exact<React$ElementConfig<typeof ZulipText>>;
 
 type Props = $ReadOnly<{|
-  ...$Exact<React$ElementConfig<typeof ZulipText>>,
+  ...BoundedDiff<
+    ZulipTextProps,
+    {|
+      // Could accept `style`, just be sure to merge with web-link style.
+      style: ZulipTextProps['style'],
+
+      onPress: ZulipTextProps['onPress'],
+    |},
+  >,
   url: URL,
 |}>;
 
@@ -28,7 +39,7 @@ const componentStyles = createStyleSheet({
  * special formatting, you have to pass `inheritColor` to those.
  */
 export default function WebLink(props: Props): React.Node {
-  const { children } = props;
+  const { url, ...zulipTextProps } = props;
 
   const globalSettings = useGlobalSelector(getGlobalSettings);
 
@@ -36,10 +47,9 @@ export default function WebLink(props: Props): React.Node {
     <ZulipText
       style={componentStyles.link}
       onPress={() => {
-        openLinkWithUserPreference(props.url.toString(), globalSettings);
+        openLinkWithUserPreference(url.toString(), globalSettings);
       }}
-    >
-      {children}
-    </ZulipText>
+      {...zulipTextProps}
+    />
   );
 }